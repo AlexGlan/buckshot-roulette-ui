f7111b4e406b155eef7ae8160647fc357322d909
Alex Glancevs <alex2531458@gmail.com>
2024-10-29 21:01:25 +0200

    feat: add constants for game state configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+export type Shell = {
+    type: string,
+    id: string
+}
+
+const MIN_LIVES: number = 3;
+const MAX_LIVES: number = 4;
+const MIN_ITEMS: number = 2;
+const MAX_ITEMS: number = 3;
+const MIN_SHELLS: number = 2;
+const MAX_SHELLS: number = 8;
+const PLAYER_ONE_NAME: string = 'Skull';
+const PLAYER_TWO_NAME: string = 'Pilot';
+const SHELL_LOCATIONS: string[] = [
+    'First',
+    'Second',
+    'Third',
+    'Fourth',
+    'Fifth',
+    'Sixth',
+    'Seventh',
+    'Eigth'
+];
+
 const App = () => {
   return (
     <></>
