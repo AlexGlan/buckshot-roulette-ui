ed918d597fa1b4d9f763c02362967bc703dc4ae4
Alex Glancevs <alex2531458@gmail.com>
2024-11-04 21:46:58 +0200

    test: add tests for util function that generates shells and loadout for the game

--- test/gameUtils.test.ts
+++ test/gameUtils.test.ts
@@ -76,3 +76,69 @@ describe('generateItems', () => {
         expect(generateItems(MIN, MAX, loadoutLength)).toEqual(expected);
     });
 });
+
+describe('generateShells', () => {
+    afterEach(() => {
+        vi.restoreAllMocks();
+    });
+
+    const MIN: number = 2;
+    const MAX: number = 8;
+
+    it('Should generate a random loadout with live and blank rounds', () => {
+        const loadout = generateShells(MIN, MAX);
+        let liveRounds: number = 0;
+        let blankRounds: number = 0;
+        for (const shell of loadout) {
+            if (shell.type === 'live') {
+                liveRounds++;
+            } else {
+                blankRounds++;
+            }
+        }
+        expect(liveRounds).toBeGreaterThan(0);
+        expect(blankRounds).toBeGreaterThan(0);
+    });
+
+    it('Should generate random loadout length within a given range', () => {
+        vi.spyOn(globalThis.Math, 'random').mockReturnValue(0.5);
+        const loadout = generateShells(MIN, MAX);        
+        expect(loadout.length).toBeGreaterThan(MIN);
+        expect(loadout.length).toBeLessThan(MAX);
+    });
+
+    it('Should generate loadout with maximum provided length (inclusive) at the upper bound', () => {
+        vi.spyOn(globalThis.Math, 'random').mockReturnValue(0.9);
+        expect(generateShells(MIN, MAX).length).toEqual(MAX);
+    });
+
+    it('Should generate loadout with minimum provided length (inclusive) at the lower bound', () => {
+        vi.spyOn(globalThis.Math, 'random').mockReturnValue(0);
+        expect(generateShells(MIN, MAX).length).toEqual(MIN);
+    });
+
+    it('Should always generate a different loadout from the previous result if previous length is provided', () => {
+        const prevLoadoutLength: number = MIN;
+        vi.spyOn(globalThis.Math, 'random').mockReturnValueOnce(0);
+        const newLoadoutLength: number = generateShells(MIN, MAX, prevLoadoutLength).length
+        expect(newLoadoutLength).not.toEqual(prevLoadoutLength);
+    });
+
+    it('Should shuffle shells in a loadout to simulate inserting them in a hidden sequence ', () => {
+        // Create two loadouts with the same number and type of shells
+        vi.spyOn(globalThis.Math, 'random').mockReturnValueOnce(0.9);
+        const firstLoadout = generateShells(MIN, MAX);
+        vi.spyOn(globalThis.Math, 'random').mockReturnValueOnce(0.9);
+        const secondLoadout = generateShells(MIN, MAX);
+        
+        let isShellOrderDifferent: boolean = false;
+        for (let i = 0; i < firstLoadout.length; i++) {
+            if (firstLoadout[i].type !== secondLoadout[i].type) {
+                isShellOrderDifferent = true;
+                break;
+            }
+        }
+        // Even though the shells in both loadouts are the same, the order should be different
+        expect(isShellOrderDifferent).toBe(true);
+    });
+});
