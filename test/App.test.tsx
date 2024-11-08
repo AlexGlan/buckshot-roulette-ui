f25abf03f993c14d5dde3b6e92049b8e9fc0f936
Alex Glancevs <alex2531458@gmail.com>
2024-11-08 18:48:34 +0200

    refactor: update test import statement path

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, within } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
-import App from '../src/App';
+import App from '../src/pages/App';
 
 describe('App', () => {
     beforeEach(() => {
