567b130a0e8428295ec84147f9c80d2e8fb88efb
Alex Glancevs <alex2531458@gmail.com>
2024-11-17 16:24:30 +0200

    test: add navigation tests

---
+++ test/components/NavBar.test.tsx
@@ -0,0 +1,21 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../test-utils";
+
+describe('NavBar', () => {
+    it('Should render route links for navigation', () => {
+        renderWithProviders();
+        expect(screen.getByRole('navigation')).toBeInTheDocument();
+        expect(screen.getByRole('link', { name: /game|play/i })).toBeInTheDocument();
+        expect(screen.getByRole('link', { name: /board/i })).toBeInTheDocument();
+    });
+
+    it('Should take user to another page when navigation link is clicked', async () => {
+        const { user } = renderWithProviders();
+        // Go to leaderboard page
+        await user.click(screen.getByRole('link', { name: /board/i }));
+        expect(screen.getByRole('button', { name: /refresh/i })).toBeInTheDocument();
+        // Go to gameplay page
+        await user.click(screen.getByRole('link', { name: /game|play/i }));
+        expect(screen.getByRole('button', { name: /start/i })).toBeInTheDocument();
+    });
+});
